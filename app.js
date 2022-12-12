d7e737b3e0e6d27b3c3ea47ec3314441ed58b981
unknown <abdullazadeniht@gmail.com>
2022-12-13 02:19:17 +0400

    initial commit

---
+++ app.js
@@ -0,0 +1,9 @@
+const btnNextSoft = document.querySelector('.hero .main button')
+
+
+btnNextSoft.addEventListener('click', btnNextSoftClick)
+
+
+function btnNextSoftClick() {
+   window.scrollTo(0, 1000)
+}
