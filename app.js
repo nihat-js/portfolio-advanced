36e7c188a56ce9e28f60d5eafaba92bc8dec4faf
unknown <abdullazadeniht@gmail.com>
2022-12-14 01:42:03 +0400

    Added about section

--- app.js
+++
@@ -1,9 +0,0 @@
-const btnNextSoft = document.querySelector('.hero .main button')
-
-
-btnNextSoft.addEventListener('click', btnNextSoftClick)
-
-
-function btnNextSoftClick() {
-   window.scrollTo(0, 1000)
-}
---
+++ js/app.js
@@ -0,0 +1,13 @@
+const btnNextSoft = document.querySelector('.hero .main button')
+btnNextSoft.addEventListener('click', btnNextSoftClick)
+function btnNextSoftClick() {
+   window.scrollTo(0, 1000)
+}
+
+// const root = document.querySelector(':root')
+// root.style.setProperty('primary-color-')
+
+
+document.body.style.background = "#000 !important"
+document.body.style.color = "#fff"
+console.log('se');
